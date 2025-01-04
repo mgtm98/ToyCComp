f316dc9510f66de8cd8287ec0cfc30a4f0bfecaf
mgtm98 <m.g.t.m.prog@gmail.com>
2025-01-04 22:21:45 +0200

    Create an easy way to use llist

---
+++ llist_definitions.c
@@ -0,0 +1,4 @@
+#include "llist_definitions.h"
+
+LLIST_DEFINE_FUNCTIONS(ASTNode_t, ASTNode)
+LLIST_DEFINE_FUNCTIONS(SymbolFuncArg_t, SymbolFuncArg)
---
+++ llist_definitions.h
@@ -0,0 +1,11 @@
+#ifndef _LLIST_DEFINITIONS_H_
+#define _LLIST_DEFINITIONS_H_
+
+#include "llist.h"
+#include "ast.h"
+#include "symtab.h"
+
+LLIST_DEFINE_PROTOTYPES(ASTNode_t, ASTNode)
+LLIST_DEFINE_PROTOTYPES(SymbolFuncArg_t, SymbolFuncArg)
+
+#endif
